cab9b9ed5e325ec6c67c9a33f78c40954d86bee3
khulanJargalsaikhan <onagraa@gmail.com>
2021-12-27 15:58:05 -0500

    Another way to start thread is to define an anonymous class inside of Thread's constructor

--- src/learningThreading/Application.java
+++ src/learningThreading/Application.java
@@ -6,40 +6,47 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Starting Thread 1");
-		Thread thread1 = new Thread(new Task("Thread-A"));
-		thread1.start();
+		Thread thread1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i=0; i<1000; i++) {
+					System.out.println("number: " + i + " - " + Thread.currentThread().getName());
+					try {
+						Thread.sleep(5);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+			
+			
+		});		thread1.start();
 		
 		System.out.println("Starting Thread 2");
-		Thread thread2 = new Thread(new Task("Thread-B"));
+		Thread thread2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i=0; i<1000; i++) {
+					System.out.println("number: " + i + " - " + Thread.currentThread().getName());
+					try {
+						Thread.sleep(5);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+			
+			
+		});
 		thread2.start();
 	}
 
 }
 
 
-
-
-
-//this is Runnable Task
-class Task implements Runnable{
-	
-	String name;
-	
-	public Task(String name) {
-		this.name = name;
-	}
-	
-	public void run() {
-		Thread.currentThread().setName(this.name);
-		for (int i=0; i<1000; i++) {
-			System.out.println("number: " + i + " - " + Thread.currentThread().getName());
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	
-}
